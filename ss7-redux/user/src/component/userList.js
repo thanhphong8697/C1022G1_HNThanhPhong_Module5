c7cac5918dd5d56544c4ac917bcc8d817e12e293
thanhphong8697 <thanhphong861997@gmail.com>
2023-04-06 21:24:51 +0700

    up casestudy

--- ss7-redux/user/src/component/userList.js
+++ ss7-redux/user/src/component/userList.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { allUsers } from "../action/user";
 
-function UserList(){
+export default function UserList(){
     const navigate = useNavigate();
 
     const users = useSelector( state => state.users)
@@ -14,5 +14,35 @@ function UserList(){
         dispatch(allUsers())
     }, []);
 
-    return
+    return (
+        <>
+        <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Website</th>
+              </tr>
+            </thead>
+            <tbody>
+                {
+                    users.map((value, index) => (
+                      <tr key={index}>
+                        <td>{value.id}</td>
+                        <td>{value.name}</td>
+                        <td>{value.email}</td>
+                        <td>{value.website}</td>
+                        <td>
+                            <button onClick={() => handleDelete(book.id)} type="button" className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                             Delete
+                            </button>
+                        </td>
+                    </tr>  
+                    ))
+                } 
+            </tbody>
+          </table>
+        </>
+    )
 }
