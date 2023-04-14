6f1c46ccc71d01321ab524a2a89a55558fd554fc
thanhphong8697 <thanhphong861997@gmail.com>
2023-04-14 20:50:51 +0700

    up casestudy

---
+++ exam-BE/demo/src/main/java/com/example/demo/config/config.java
@@ -0,0 +1,17 @@
+package com.example.demo.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class config {
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
