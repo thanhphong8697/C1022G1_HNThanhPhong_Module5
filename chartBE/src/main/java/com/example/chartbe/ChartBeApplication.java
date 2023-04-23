e664ef7b986f25375f044ab0ad2d359f03618824
thanhphong8697 <thanhphong861997@gmail.com>
2023-04-23 23:34:33 +0700

    first commit

---
+++ chartBE/src/main/java/com/example/chartbe/ChartBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.chartbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChartBeApplication.class, args);
+    }
+
+}
---
+++ chartBE/src/test/java/com/example/chartbe/ChartBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chartbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
