57e5ee46fb717b8e73ace3401d3797ee2e2d8aa9
thanhphong8697 <thanhphong861997@gmail.com>
2023-04-23 23:56:17 +0700

    first commit

--- chart_fe/src/App.js
+++ chart_fe/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { useState } from "react";
+import Data from "./data";
+import PieChart from "./component/PieChart";
+import BarChart from "./component/BarChart";
+import LineChart from "./component/LineChart";
 
-function App() {
+Chart.register(CategoryScale);
+ 
+export default function App() {
+  const [chartData, setChartData] = useState({
+    labels: Data.map((data) => data.year), 
+    datasets: [
+      {
+        label: "Users Gained ",
+        data: Data.map((datas) => datas.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      }
+    ]
+  });
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+      <PieChart chartData={chartData} />
+      <BarChart chartData={chartData} />
+      <LineChart chartData={chartData} />
+    </div>
     </div>
   );
 }
-
-export default App;
